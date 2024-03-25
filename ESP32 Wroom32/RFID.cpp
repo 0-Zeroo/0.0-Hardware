65464e19a0e19295f1adc9fe33733cdb6e363f8e
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-03-26 01:45:29 +0900

    REFACTOR : LOLIN D32

--- ESP32 Wroom32/RFID.cpp
+++ ESP32 Wroom32/RFID.cpp
@@ -8,9 +8,9 @@
 #define WIFI_SSID "비밀번호 12345678"
 #define WIFI_PASSWORD "dongwook"
 #define API_KEY "AIzaSyDtHBVBmcG3gcYxhhBXD0DceS8QVymgm8M"
-#define DATABASE_URL "https://umbrella-system-6eeb4-default-rtdb.firebaseio.com"
+#define DATABASE_URL "umbrella-system-6eeb4-default-rtdb.firebaseio.com"
 #define SS_PIN 5
-#define RST_PIN 0
+#define RST_PIN 22
 #define RFID_Code_Size 4
 #define KEY_Size 6
 
@@ -39,17 +39,11 @@ void setup() {
     for (int i = 0; i < KEY_Size; i++) {
         KEY.keyByte[i] = 0xFF;
     }
-    Serial.println(F("This code scan the MIFARE Classsic NUID."));
-    Serial.print(F("Using the following key:"));
-    //printHex(KEY.keyByte, MFRC522::MF_KEY_SIZE);
 
     Config.api_key = API_KEY;
     Config.database_url = DATABASE_URL;
     if (Firebase.signUp(&Config, &Auth, "", "")) {
         Serial.println("ok");
-    }
-    else {
-        Serial.printf("%s\n", Config.signer.signupError.message.c_str());
     }
     Config.token_status_callback = tokenStatusCallback;
     Firebase.begin(&Config, &Auth);
@@ -76,7 +70,7 @@ void loop() {
             RFID_Code[i] = RFID.uid.uidByte[i];
         }
         Serial.println(F("The NUID tag is:"));
-        /*
+        
         Serial.print(F("In hex: "));
         printHex(RFID.uid.uidByte, RFID.uid.size);
         Serial.println();
@@ -84,16 +78,17 @@ void loop() {
         Serial.print(F("In dec: "));
         printDec(RFID.uid.uidByte, RFID.uid.size);
         Serial.println();
-        */
-        Firebase.RTDB.setInt(&Fbdo, "/ESP32", FirebaseString());
+        long m=FirebaseString();
+        Firebase.RTDB.setInt(&Fbdo, "/ESP32", m);
+        Serial.println(m);
     }
     else Serial.println(F("Card read previously."));
-    RFID.PICC_HaltA();
+    RFID.PICC_HaltA();  
     RFID.PCD_StopCrypto1();
 }
 
-long long FirebaseString() {
-    long long A;
+long FirebaseString() {
+    long A=0;
     for (int i=0;i<RFID_Code_Size;i++){
         A*=1000;
         A+=RFID_Code[i];
@@ -101,7 +96,7 @@ long long FirebaseString() {
     return A;
 }
 
-/*
+
 void printHex(byte *buffer, byte bufferSize) {
     for (byte i = 0; i < bufferSize; i++) {
         Serial.print(buffer[i] < 0x10 ? " 0" : " ");
@@ -115,4 +110,3 @@ void printDec(byte *buffer, byte bufferSize) {
         Serial.print(buffer[i], DEC);
     }
 }
-*/
